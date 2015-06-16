f5c5ee68c33f7711362e0e54239d28cc2096be37
William Durand <will+git@drnd.me>
2015-06-16 11:28:40 +0200

    Revert "install john"
    This reverts commit a9922e06e15c1219e4555cd526c58c9ac775a3e0.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
-#!/bin/bash -
+#!/bin/bash - 
 #===============================================================================
 #
 #          FILE: bootstrap.sh
-#
-#         USAGE: ./bootstrap.sh
-#
-#   DESCRIPTION:
-#
+# 
+#         USAGE: ./bootstrap.sh 
+# 
+#   DESCRIPTION: 
+# 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: YOUR NAME (),
-#  ORGANIZATION:
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
 #       CREATED: 24.02.2014 09:23
 #      REVISION:  ---
 #===============================================================================
@@ -24,7 +24,7 @@ apt-get update
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password vmdev'
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password vmdev'
 sudo apt-get update
-sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 vim john
+sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 vim
 
 rm -rf /var/www
 ln -s /vagrant/dvwa-1.0.8 /var/www
